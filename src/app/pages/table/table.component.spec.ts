9b21b17fa8de9a522ce07009290eb4e6fb2de3ef
Alexdev2019 <alexpetrov248@gmail.com>
2021-02-28 23:48:23 +0200

    Created some unit tests for Table component

--- src/app/pages/table/table.component.spec.ts
+++ src/app/pages/table/table.component.spec.ts
@@ -1,25 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TableComponent } from './table.component';
+import {TableComponent} from './table.component';
+import {TableDataService} from './table-data.service';
+import {TestBed} from '@angular/core/testing';
 
 describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+  let tableData;
+  let service: TableDataService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TableComponent ]
-    })
-    .compileComponents();
+  const json = '[{"name":"Name 1","year":"2010"},' +
+  '{"name":"Name 2","year":"1997"},{"name":"Name 3","year":"2004"}]';
+
+  beforeEach( () => {
+    TestBed.configureTestingModule({
+      providers: [TableDataService],
+    });
+
+    tableData = {
+      checkData: jest.fn(),
+    };
+
+    service = TestBed.inject(TableDataService);
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('should checkData to be called', () => {
+      const checkData = jest.spyOn(tableData, 'checkData');
+      tableData.checkData();
+      expect(checkData).toHaveBeenCalledTimes(1);
+    });
   });
+  describe('Service', () => {
+    it('should check saveJSON function to be called', () => {
+      const saveJSON = jest.spyOn(service, 'saveJSON');
+      service.saveJSON(json);
+      expect(saveJSON).toHaveBeenCalledTimes(1);
+    });
+
+    it('should check loadJSON function value', () => {
+      expect(service.loadJSON()).toBe(json);
+    });
+  });
+  describe('', () => {});
 });
